a5fcaeaa456c07186227ff06e1eb45d27e73db15
Viviana Harrizon <viviana.harrizon@gmail.com>
2023-06-26 18:12:03 -0600

    selección de mascotas jugadores y enemigos

--- js/mokepon.js
+++ js/mokepon.js
@@ -7,14 +7,34 @@ function seleccionarMascotaJugador(){
     let inputHypodoge = document.getElementById("hypodoge");
     let inputCapipepo = document.getElementById("capipepo");
     let inputRatigueya =document.getElementById("ratigueya");
+    let spanMascotaJugador = document.getElementById("mascota-jugador");
+ 
 
     if (inputHypodoge.checked){
-        alert("Usted ha seleccionado a Hypodoge")
+        spanMascotaJugador.innerHTML = 'Hypodoge '
     }else if (inputCapipepo.checked){
-        alert("Usted ha seleccionado a Capipepo")
+        spanMascotaJugador.innerHTML = 'Capipepo ';
     }else if (inputRatigueya.checked){
-        alert("Usted ha seleccionado a Ratigueya")
+     spanMascotaJugador.innerHTML = 'Ratigueya ';
+    }else {
+        alert('Selecciona una masccota');
+    }
+    seleccionarMascotaEnemigo()
+}
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
+
+    if(ataqueAleatorio == 1){
+        spanMascotaEnemigo.innerHTML = 'Hypodogue'
+    }else if (ataqueAleatorio == 2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    }else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
     }
 }
+function aleatorio (min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 
 window.addEventListener('load',iniciarJuego);
