71ffa3de4349556fd6de69cdd45d85f9a31d3d50
Viviana Harrizon <viviana.harrizon@gmail.com>
2023-06-27 18:00:00 -0600

    Asisnación de modo de ataque mascota jugador y enemigo

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,40 +1,102 @@
-function iniciarJuego(){
-    let botonMascotaJugador = document.getElementById('boton-mascota');
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+let ataqueJugador
+let ataqueEnemigo
+
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
 }
 
-function seleccionarMascotaJugador(){
-    let inputHypodoge = document.getElementById("hypodoge");
-    let inputCapipepo = document.getElementById("capipepo");
-    let inputRatigueya =document.getElementById("ratigueya");
-    let spanMascotaJugador = document.getElementById("mascota-jugador");
- 
-
-    if (inputHypodoge.checked){
-        spanMascotaJugador.innerHTML = 'Hypodoge '
-    }else if (inputCapipepo.checked){
-        spanMascotaJugador.innerHTML = 'Capipepo ';
-    }else if (inputRatigueya.checked){
-     spanMascotaJugador.innerHTML = 'Ratigueya ';
-    }else {
-        alert('Selecciona una masccota');
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = 'Hipodoge'
+    } else if (inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = 'Capipepo'
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    } else {
+        alert('Selecciona una mascota')
     }
+
     seleccionarMascotaEnemigo()
 }
-function seleccionarMascotaEnemigo(){
-    let ataqueAleatorio = aleatorio(1,3)
-    let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
+function seleccionarMascotaEnemigo() {
+    let mascotaAleatoria = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
 
-    if(ataqueAleatorio == 1){
-        spanMascotaEnemigo.innerHTML = 'Hypodogue'
-    }else if (ataqueAleatorio == 2){
+    if (mascotaAleatoria == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (mascotaAleatoria == 2) {
         spanMascotaEnemigo.innerHTML = 'Capipepo'
-    }else {
+    } else {
         spanMascotaEnemigo.innerHTML = 'Ratigueya'
     }
 }
+
+function ataqueFuego() {
+    ataqueJugador = 'FUEGO'
+    ataqueAleatorioEnemigo()
+}
+function ataqueAgua() {
+    ataqueJugador = 'AGUA'
+    ataqueAleatorioEnemigo()
+}
+function ataqueTierra() {
+    ataqueJugador = 'TIERRA'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3);
+    
+    if (ataqueAleatorio == 1){
+        ataqueEnemigo = 'FUEGO'
+    }else if(ataqueAleatorio == 2){
+        ataqueEnemigo = 'AGUA'
+    }else{
+        ataqueEnemigo = 'TIERRA'
+    }
+    combate();
+}
+function combate() {
+    if(ataqueEnemigo == ataqueJugador) {
+        crearMensaje("EMPATE")
+    } else if(ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA') {
+        crearMensaje("GANASTE")
+    } else if(ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO') {
+        crearMensaje("GANASTE")
+    } else if(ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA') {
+        crearMensaje("GANASTE")
+    } else {
+        crearMensaje("PERDISTE")
+    }
+}
+function crearMensaje(resultado){
+    let sectionMensajes = document.getElementById('mensajes');
+
+    let parrafo = document.createElement('p');
+    parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ', la mascota del enemigo ataco con '  + ataqueEnemigo + ' -- 🎉🎉🎉' + resultado;
+    sectionMensajes.appendChild(parrafo);
+}
+
 function aleatorio (min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }  
 
 window.addEventListener('load',iniciarJuego);
+
+
+
+
+
